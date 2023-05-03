7070ab6a09e11ee63a54f4c6702caaa603a62f85
Aaron Jack <apietsch4@gmail.com>
2023-05-02 17:59:52 -1000

    fmt changes

--- btc.sh
+++ btc.sh
@@ -25,24 +25,54 @@ btc_data=$(bitcoin-chart-cli --toplist 1)
 # Use sed to extract the dollar value
 price=$(echo "$btc_data" | sed -n 's/.*\$\([0-9,\.]*\) .*/\1/p' | tr -d ',')
 
-# Multiply the price by the quantity
-value=$(echo "$price * $quantity" | bc)
+# Calculate the total value if quantity is provided
+if [[ -n "$quantity" ]]; then
+  # Multiply the price by the quantity
+  value=$(echo "$price * $quantity" | bc)
+
+  # Use sed to extract the percentage change
+  change_percent=$(echo "$btc_data" | sed -n 's/.* \([0-9\.]*%\)$/\1/p' | tr -d '%')
+
+  # Extract the numerical value of the percentage change
+  change_decimal=$(echo "$change_percent" | sed 's/\([0-9.]*\)%/\1/' | awk '{print $1/100}')
+
+  # Calculate the dollar value of the percentage increase
+  prev_value=$(printf "%.2f" $(echo "$value / (1 + ($change_decimal))" | bc))
+  increase=$(printf "%.2f" $(echo "$value - $prev_value" | bc))
+
+  # Format the increase with a comma for thousands separator
+  increase_formatted=$(printf "%'.0f" "$increase")
+
+else
+  # If quantity is not provided, set value and increase to zero
+  value=0
+  increase_formatted=0
+fi
 
 # Use sed to extract the percentage change
-change=$(echo "$btc_data" | sed -n 's/.* \([0-9\.]*%\)$/\1/p' | tr -d '%')
+change_percent=$(echo "$btc_data" | sed -n 's/.* \([0-9\.]*%\)$/\1/p' | tr -d '%')
 
 # Determine the color for the percentage change
-if [ -n "$change" ] && [ $(echo "$change < 0" | bc) -eq 1 ]; then
+if [ -n "$change_percent" ] && [ $(echo "$change_percent < 0" | bc) -eq 1 ]; then
     color="\e[31m"  # red
 else
     color="\e[32m"  # green
 fi
 
+  # Add a plus or minus symbol before the increase depending on its sign
+  if [ $(echo "$increase >= 0" | bc) -eq 1 ]; then
+    increase_formatted="+\$$increase_formatted"
+    change_percent="+$change_percent"
+  else
+    increase_formatted="-\$$increase_formatted"
+  fi
+
 # Format and print the formatted, colorized result
 price_formatted=$(printf "%'.0f" "$price")
 value_formatted=$(printf "%'.0f" "$value")
+increase_formatted=$(echo "$increase_formatted" | tr -d ',')
 
-printf "\e[32mCurrent Price:\e[0m \e[33m\$%s\e[0m\n" "$price_formatted"
-printf "\e[32mTotal Value:\e[0m \e[33m\$%s\e[0m\n" "$value_formatted"
-printf "${color}Change/24h: %s%%\e[0m\n" "$change"
+printf "Current Price:\e[0m \e[33m\$%s\e[0m\n" "$price_formatted"
+printf "Total Value:\e[0m \e[33m\$%s\e[0m\n" "$value_formatted"
+printf "Change/24h: ${color}%s%% / %s\n" "$change_percent" "$increase_formatted" | sed "s/\([0-9]\)\([0-9]\{3\}\)/\1,\2/g"
 
